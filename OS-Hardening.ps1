b5d15ee7ae72a98888fede164ea4ba97dbe4246e
Nish B <butta28@usf.edu>
2024-12-06 22:30:25 -0500

    test commit

--- OS-Hardening.ps1
+++ OS-Hardening.ps1
@@ -91,4 +91,4 @@ reg add "HKLM\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protoc
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Client" /v Enabled /t REG_DWORD /d 1 /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Server" /v Enabled /t REG_DWORD /d 1 /f
 
-#
+# Test
